57c6397c239757e2d7074147bd510a9fdd4f6fd4
hamdan <hamdandirgantara@gmail.com>
2025-10-23 14:21:16 +0700

    feat: food info popup

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,59 @@ import Sidebar from "./components/sidebar";
 import { useSidebar } from "./components/context-providers/sidebar";
 
 interface Meal {
+  idMeal: string;
   strMeal: string;
+  strMealAlternate: string | null;
+  strCategory: string;
+  strArea: string;
+  strInstructions: string;
   strMealThumb: string;
+  strTags: string | null;
+  strYoutube: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  strIngredient11: string;
+  strIngredient12: string;
+  strIngredient13: string;
+  strIngredient14: string;
+  strIngredient15: string;
+  strIngredient16: string;
+  strIngredient17: string;
+  strIngredient18: string;
+  strIngredient19: string;
+  strIngredient20: string;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strMeasure9: string;
+  strMeasure10: string;
+  strMeasure11: string;
+  strMeasure12: string;
+  strMeasure13: string;
+  strMeasure14: string;
+  strMeasure15: string;
+  strMeasure16: string;
+  strMeasure17: string;
+  strMeasure18: string;
+  strMeasure19: string;
+  strMeasure20: string;
+  strSource: string;
+  strImageSource: string | null;
+  strCreativeCommonsConfirmed: string | null;
+  dateModified: string | null;
 }
 interface SearchData {
   meals: Meal[] | null;
@@ -17,10 +68,34 @@ function App() {
   const { sidebar } = useSidebar();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [popupState, setPopupState] = useState<"block" | "none">("none");
+  const [popupData, setPopupData] = useState<Meal | null>(null);
   const [searchData, setSearchData] = useState<SearchData>({ meals: null });
   let prevTimer: any = null;
   function handleChangeTheme() {}
   function handleChangeLanguage() {}
+  function renderIngredients(meal: Meal) {
+    let ingredientsElements = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = meal[`strIngredient${i}` as keyof typeof meal];
+      const measure = meal[`strMeasure${i}` as keyof typeof meal];
+      if (ingredient && ingredient?.trim() !== "") {
+        ingredientsElements.push(
+          <li key={i}>
+            {measure?.trim()} {ingredient?.trim()}
+          </li>,
+        );
+      }
+    }
+    return ingredientsElements;
+  }
+  function toggleFoodInfoPopupState() {
+    setPopupState(popupState === "block" ? "none" : "block");
+  }
+  function handleFoodInfoPopup(items: Meal) {
+    setPopupData(items);
+    toggleFoodInfoPopupState();
+  }
   function handleSearch(event: any) {
     prevTimer && clearTimeout(prevTimer);
     prevTimer = setTimeout(() => {
@@ -87,18 +162,53 @@ function App() {
       <div className="sidebar-container" style={{ display: sidebar }}>
         <Sidebar />
       </div>
+      <div className="food-popup-container" style={{ display: popupState }}>
+        {popupData ? (
+          <div style={{display:"flex", flexDirection:"column", alignItems:"center", gap:"10px"}}>
+            <iframe
+							style={{width:"90%", height:"500px"}}
+              src={`https://www.youtube.com/embed/${popupData.strYoutube.split("v=")[1]}`}
+              title="YouTube video player"
+              allow="accelerometer; 
+  autoplay; 
+  clipboard-write; 
+  encrypted-media; 
+  gyroscope; 
+  picture-in-picture; 
+  web-share"
+              allowFullScreen
+            ></iframe>
+            <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+              <div>
+                <div>{popupData.strMeal}</div>
+                <div>{popupData.strArea}</div>
+              </div>
+              <img src={popupData.strMealThumb} alt={popupData.strMeal} height={100} width={100}></img>
+            </div>
+            <ul>{renderIngredients(popupData)}</ul>
+          </div>
+        ) : (
+          <div>null</div>
+        )}
+        <div></div>
+      </div>
       {searchData.meals ? (
         <div className="item-list">
           {searchData.meals.map((items: Meal) => {
             return (
-              <div className="food-container">
-                <div className="food-name">{items.strMeal}</div>
-                <img
-                  src={items.strMealThumb}
-                  height={100}
-                  className="food-thumbs"
-                ></img>
-              </div>
+              <button
+                title={items.strMeal}
+                onClick={() => handleFoodInfoPopup(items)}
+              >
+                <div className="food-container">
+                  <div className="food-name">{items.strMeal}</div>
+                  <img
+                    src={items.strMealThumb}
+                    height={100}
+                    className="food-thumbs"
+                  ></img>
+                </div>
+              </button>
             );
           })}
         </div>
